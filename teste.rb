f0a2b273cf674cafd99fb80e9c0356431e4a0969
Kelson <kelsonbatista@gmail.com>
2023-10-30 19:35:17 -0300

    File deleted

--- teste.rb
+++
@@ -1,9 +0,0 @@
-require './assert.rb'
-require './produto.rb'
-require_relative './assert_namespace'
-
-produto = Produto.new "Capacete", 550.0
-
-Tabajara::Assert.assert produto.nome == "Capacete"
-Tabajara::Assert.assert produto.preco == 550.0
-
