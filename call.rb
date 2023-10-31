41bec29f4fbfda1a5c4979f968b005fa6a03bdab
Kelson <kelsonbatista@gmail.com>
2023-10-30 21:53:43 -0300

    anonymous function with Proc class

--- call.rb
+++ call.rb
@@ -7,5 +7,8 @@ end
 
 # hello("world") { |x| puts "dentro do bloco: #{x}" }
 
-ola = -> (quem) { puts "olá #{quem}" } # funções anonimas
-ola.call("mundo")
+# ola = -> (quem) { puts "olá #{quem}" } # funções anonimas
+# ola.call("mundo")
+
+ola = Proc.new { |quem| puts "olá #{quem}" }
+ola.call "mundo"
