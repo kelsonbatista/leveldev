484e3d802d1028b113f78ba5bcf020a76dcd79eb
Kelson <kelsonbatista@gmail.com>
2023-10-30 21:49:14 -0300

    xpto call

--- call.rb
+++ call.rb
@@ -1,7 +1,8 @@
-def hello(what)
+def hello(what, &xpto)
     puts "hello #{what}"
 
-    yield "mundo"
+    # yield "mundo"
+    xpto.call("mundo") # invocação de forma explicita com call
 end
 
 hello("world") { |x| puts "dentro do bloco: #{x}" }
